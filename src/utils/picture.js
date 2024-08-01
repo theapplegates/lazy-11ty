029511bb4c7d462d75c7f5cb9dfd4febc47cac13
Paul <paul@theapplegates.com>
2024-08-01 10:04:48 -0400

    first commit

---
+++ src/utils/picture.js
@@ -0,0 +1,8 @@
+module.exports = (url, alt = "Missing alt text") => {
+  return `
+    <picture>
+      <source srcset="${url}" type="image/jpeg" sizes="(min-width: 1200px) 1200px, (min-width: 900px) 900px, (min-width: 600px) 600px, 400px" />
+      <img src="${url}" alt="${alt}" />
+    </picture>
+  `;
+};
